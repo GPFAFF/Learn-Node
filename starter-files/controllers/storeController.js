f3ebfd1bf1310e54eb6c93c80a6f3b52d16364c1
Greg Pfaff <greg.pfaff@sapientrazorfish.com>
2018-10-30 21:21:19 -0500

    adding store redirect

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -4,6 +4,8 @@ const Store = mongoose.model('Store');
 exports.homePage = (req, res) => {
   console.log(req.name);
   const dogs = {name1: 'Eisor', name2: 'Repooc', title: 'Fuck'}
+  req.flash('error', `Shit is broken!`);
+
   res.render('index', {...dogs});
 }
 
@@ -12,7 +14,8 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
+  const store = await(new Store(req.body)).save();
   await store.save();
-  res.redirect('/');
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 }
