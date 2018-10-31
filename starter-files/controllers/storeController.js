458ba833275e3e3842a21d75953760a2dc43d4f4
Greg Pfaff <greg.pfaff@sapientrazorfish.com>
2018-10-30 22:34:21 -0500

    adding form updates and store ids

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -19,3 +19,19 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 }
+
+exports.getStores = async (req, res) => {
+  // Query the database for a list of all stores
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', {title: 'stores', stores});
+}
+
+exports.editStore = async (req, res) => {
+  // Find store given id
+  // Confirm they are owner of store
+  // Render out edit form so user can update page
+  const store = await Store.findOne({ _id: req.params.id});
+  res.render('editStore', {title: `Edit ${store.name}`, store});
+}
+
